a088e0c7103901b86cb591c8067d5dc4b050ee8d
DaSaltySquid <104535317+DaSaltySquid@users.noreply.github.com>
2023-05-12 11:06:55 -0400

    Started workshop

---
+++ src/main/java/com/learntocode/Dealership.java
@@ -0,0 +1,20 @@
+package com.learntocode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    private List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    private void addVehicles(Vehicle vehicle) {
+
+    }
+}
